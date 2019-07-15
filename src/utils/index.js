e2c08d3d3902b448e86cc4a53076cf5841307dd2
NengGe <1765862089@qq.com>
2019-07-15 14:47:57 +0800

    增加缓存处理函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 export let isArray = function() {
   return Object.prototype.toString.call(arr) === '[object Array]'
 }
+// 缓存函数
+export let cache = function(fn) {
+  let cache = {}
+  return function(str) {
+    let hit = cache[str]
+    return hit || (cache[str] = fn(str))
+  }
+}
