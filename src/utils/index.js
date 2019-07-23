34817bded1a13ec6292f7ef5de8c4278e304c1c6
NengGe <1765862089@qq.com>
2019-07-23 16:17:26 +0800

    增加缓动函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,15 @@
 export let isArray = function() {
   return Object.prototype.toString.call(arr) === '[object Array]'
 }
+/**
+ *
+ * @param {*} type 数据类型 首字母需要大写
+ * @param {*} target 需要判定数据类型的对象
+ */
+export let isType = (type, target) => {
+  return `[object ${type}]` === Object.prototype.toString.call(target)
+}
+
 // 缓存函数
 export let cache = function(fn) {
   let cache = {}
@@ -10,7 +19,35 @@ export let cache = function(fn) {
   }
 }
 
-// 缓动函数，可做动画，先快后慢
+/**
+ *  先快后慢的缓动函数
+ * @param {*} start 缓动起始位置
+ * @param {*} end 缓动目标位置
+ * @param {*} rate 缓动比例
+ * @param {*} callback 每次缓动后执行的回调件函数
+ */
+export let easeOut = function({ start = 0, end = 0, rate = 2, callback } = {}) {
+  let isAnimating = true
+  if (!window.removeEventListener) {
+    requestAnimationFrame = function(fn) {
+      setTimeout(fn, 17)
+    }
+  }
+  if (start === end) {
+    return
+  }
+  let step = function() {
+    start += (end - start) / rate
+    if (Math.abs(end - start) < 1) {
+      isAnimating = false
+      typeof callback === 'function' && callback(end, isAnimating)
+      return
+    }
+    typeof callback === 'function' && callback(start, isAnimating)
+    requestAnimationFrame(step)
+  }
+  step()
+}
 /**
  * 排序函数
  * @param {*} param word表示数组元素是对象时需要排序的key desc--->降序 asc--->升序
@@ -25,6 +62,7 @@ export let insertSort = function({ targetArr = [], word = '', order = 'desc' } =
     let element = targetArr.shift()
     let isObject = typeof element === 'object'
     let ele = Number(isObject ? element[word] : element)
+
     let minIndex = newArr.findIndex((item, index) => {
       let itemValue = isObject ? Number(item[word]) : Number(item)
       return order === 'desc' ? itemValue < ele : itemValue > ele
