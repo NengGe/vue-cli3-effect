936690db5935a3c726fc461cdca617d042318488
NengGe <1765862089@qq.com>
2019-09-04 20:28:34 +0800

    pc惯性滚动组件开发

--- src/components/pc-scroll/index.vue
+++ src/components/pc-scroll/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="scroll-wrapper">
     <div class="scroll-content" ref="content">
-      scrollvxvxccccccccccccccccccccccccccccccccccccccdfdslfjkdslfjdslkfjdskfjdskfjdskfjdskfjdskfjkdsfjdskfkdsdkfsdkjfdskfjdskfjdskfjdsksdlfksdjflsfjlkdsjflksjfkdsljfkdslfjksdcccccccccccccccccccccccccccccccccccccccccccccccccccccsdjflsjflkdsfjsdlfjsccccccccccccccccccc
+      <div class="img-box" style="width: 2000px;" ref="imgBox">
+        <img src="./assets/img/1.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/2.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/3.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/4.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/5.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/6.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/7.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/8.jpg" alt="" width="300px" height="200px" />
+        <img src="./assets/img/9.jpg" alt="" width="300px" height="200px" />
+      </div>
     </div>
   </div>
 </template>
@@ -10,8 +20,7 @@
 import PcScroll from '@/utils/dom/pc-scroll'
 export default {
   data() {
-    return {
-    }
+    return {}
   },
   created() {
     // console.log(prefixStyle('transform'))
@@ -21,6 +30,12 @@ export default {
     setTimeout(() => {
       let el = this.$refs.content
       let pcscroll = new PcScroll({ el })
+      let imgList = document.querySelectorAll('img')
+      Array.prototype.forEach.call(imgList, function(ele, item) {
+        ele.addEventListener('mousedown', (ev) => {
+          ev.preventDefault()
+        })
+      })
       console.log(pcscroll)
     }, 17)
   }
@@ -30,11 +45,30 @@ export default {
 .scroll-wrapper {
   width: 960px;
   margin: 100px auto;
-  height: 260px;
+  // height: 260px;
   border: 1px solid #cccccc;
+  cursor: pointer;
   overflow: hidden;
   .scroll-content {
     height: 100%;
   }
+  .img-box {
+    display: flex;
+    height: 260px;
+    flex-wrap: nowrap;
+    align-items: center;
+    & > img {
+      margin-left: 20px;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -khtml-user-select: none;
+      -ms-user-select: none;
+
+      /* 以下两个属性目前并未支持，写在这里为了减少风险 */
+      -o-user-select: none;
+      user-select: none;
+    }
+  }
 }
+
 </style>
--- src/utils/dom/pc-scroll.js
+++ src/utils/dom/pc-scroll.js
@@ -1,24 +1,33 @@
 
 import prefixStyle from '@/utils/prefixStyle'
+// import { getStyle } from '@/utils'
 class PcScroll {
-  constructor({ el, direction = 'x' } = {}) {
+  constructor({ el, direction = 'x', downNumber = 20 } = {}) {
     this.$el = el
+    this.downNumber = downNumber // 调控速度的系数,越大速度越快,惯性距离越长
     this.direction = direction
     this.$el && this.init()
   }
   init() {
     this.parent = this.$el.parentNode
     this.start = 0
+    this.startTime = 0
+    this.endTIme = 0
     this.move = 0
-    this.isMove = false
+    this.isMove = false // 是否执行移动回调函数的判断条件
+    this.initPostion = 0 // 记录每次开始滑动的时候起始点坐标,算释放鼠标点击时候的速度有用
+    this.timer = null // 是否执行惯性动画
+    this.isforceMove = false // 没有出发mousemove事件不能触发惯性滚动(一次瞬时点击会触发惯性滚动)
+    this.dis = 0
     // this.setTransition()
+    this.setTransfrom(0)
     this.bindEvent()
   }
   setTransition(time = 200) {
     let preTransition = prefixStyle('transition')
     this.$el.style[preTransition] = `all ${time}ms`
   }
-  setTransfrom(dis) {
+  setTransfrom(dis = 0) { // 设置transform值
     let { direction } = this
     let translateStr = direction === 'x' ? `translate3d(${dis}px, 0, 0)` : `translate3d(0, ${direction}px, 0)`
     this.$el.style[prefixStyle('transform')] = translateStr
@@ -27,34 +36,64 @@ class PcScroll {
     let { $el, mousedownHandle, mouseupHandle, parent, direction } = this
     let parentSize = direction === 'x' ? parent.clientWidth : parent.clientHeight
     let scrollSize = direction === 'x' ? $el.scrollWidth : $el.scrollHeight
-    if (scrollSize <= parentSize) return null // 子容器尺寸不超过父容器时候不能移动
+    if (scrollSize <= parentSize) return null // 滚动容器尺寸不超过父容器时候不能移动
+
+    $el.style[direction === 'x' ? 'width' : 'height'] = scrollSize + 'px' // 设置滚动容器宽度为内容宽度
     $el.addEventListener('mousedown', mousedownHandle.bind(this), false)
-    $el.addEventListener('mouseup', mouseupHandle.bind(this), false)
+    window.addEventListener('mouseup', mouseupHandle.bind(this), false)
   }
-  mousedownHandle(event) {
+  mousedownHandle(event) { // 鼠标向下点击时回调
     let { $el, mousemoveHandle } = this
-    this.isMove = true
     event = event || window.event
-    this.start = this.direction === 'x' ? event.clientX : event.clientY
+    this.isMove = true
+    this.timer = false // 一点击就禁止掉惯性动画
+    this.startTime = new Date().getTime()
+    this.initPostion = this.start = this.direction === 'x' ? event.clientX : event.clientY // 记录起始点位置
     $el.addEventListener('mousemove', mousemoveHandle.bind(this), false)
   }
-  mousemoveHandle(event) {
-    if (this.isMove) {
-      event = event || window.event
-      this.move = this.direction === 'x' ? event.clientX : event.clientY
-      let dis = this.move - this.start
-      this.setTransfrom(dis)
-    }
+  mousemoveHandle(event) { // 鼠标移动时回调
+    if (!this.isMove) return
+    // let { $el } = this
+    event = event || window.event
+    this.isforceMove = true
+    this.move = this.direction === 'x' ? event.clientX : event.clientY
+    this.dis += this.move - this.start // 每次移动的距离
+    this.start = this.move
+    this.setTransfrom(this.dis)
   }
-  mouseupHandle() {
+  mouseupHandle() { // 鼠标释放时候回调
     this.isMove = false
+    this.endTIme = new Date().getTime()
+    if (!this.isforceMove) return
+    this.isforceMove = false
+    if (this.endTIme - this.startTime > 300) return null
+    let { initPostion, move, startTime, endTIme, downNumber } = this
+    let speed = ((move - initPostion) / (endTIme - startTime)) * downNumber // 释放鼠标时候的瞬时速度,模拟算法
+    this.timer = true
+    let moveEase = () => {
+      let f = Math.min(Math.abs(speed) / 16, 1.6) // 摩擦力有极限,也就是加速度是有极限的,因此速度删减也是有极限的,这里指定极限是0.9
+      // console.log(speed)
+      this.timer && requestAnimationFrame(moveEase)
+      if (speed < -0.3) {
+        speed += f
+      } else if (speed > 0.4) {
+        speed -= f
+      } else {
+        speed = 0
+        this.timer = false
+      }
+      this.dis += speed
+      this.setTransfrom(this.dis)
+    }
+    moveEase()
+    // console.log(speed)
     this.removeEvent()
   }
-  removeEvent() { // 接触监听事件
+  removeEvent() { // 解除监听事件
     let { $el, mousedownHandle, mouseupHandle, mousemoveHandle } = this
     $el.removeEventListener('mousedown', mousedownHandle)
     $el.removeEventListener('mousemove', mousemoveHandle)
-    $el.removeEventListener('mouseup', mouseupHandle)
+    window.removeEventListener('mouseup', mouseupHandle)
   }
 }
 export default PcScroll
